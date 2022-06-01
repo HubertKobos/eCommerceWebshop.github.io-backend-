afa8865518122042f93e2a52e443313370f14fca
Hubert <hkobos63@gmail.com>
2022-06-01 16:53:22 +0200

    backend

---
+++ backend/base/products.py
@@ -0,0 +1,74 @@
+products = [
+    {
+        'id': '1',
+        'name': 'Kamień biały',
+        'image': '../resources/kamien_jeden.jpg',
+        'description': 'Aenean in velit et tellus ullamcorper lacinia. Proin eu orci rutrum, mollis felis sed, vehicula magna. Aliquam erat volutpat. Sed dolor sem, rhoncus ut faucibus convallis, blandit sit amet diam.',
+        'price': 34.99,
+        'countInStock': 1000,
+        'rating': 4.5,
+        'numReviews': 10
+    },
+    {
+        'id': '2',
+        'name': 'Kamień czerwony',
+        'image': '../resources/02.jpg',
+        'description': 'Aenean in velit et tellus ullamcorper lacinia. Proin eu orci rutrum, mollis felis sed, vehicula magna. Aliquam erat volutpat. Sed dolor sem, rhoncus ut faucibus convallis, blandit sit amet diam.',
+        'price': 51.99,
+        'countInStock': 1500,
+        'rating': 5,
+        'numReviews': 8
+    },
+    {
+        'id': '3',
+        'name': 'Kamień brązowy',
+        'image': '../resources/03.jpg',
+        'description': 'Aenean in velit et tellus ullamcorper lacinia. Proin eu orci rutrum, mollis felis sed, vehicula magna. Aliquam erat volutpat. Sed dolor sem, rhoncus ut faucibus convallis, blandit sit amet diam.',
+        'price': 12.99,
+        'countInStock': 4050,
+        'rating': 3.5,
+        'numReviews': 2
+    },
+    {
+        'id': '4',
+        'name': 'Kamień jasny',
+        'image': '../resources/04.jpg',
+        'description': 'Aenean in velit et tellus ullamcorper lacinia. Proin eu orci rutrum, mollis felis sed, vehicula magna. Aliquam erat volutpat. Sed dolor sem, rhoncus ut faucibus convallis, blandit sit amet diam.',
+        'price': 100.99,
+        'countInStock': 800,
+        'rating': 4.0,
+        'numReviews': 4
+    },
+    {
+        'id': '5',
+        'name': 'Kamień fioletowy',
+        'image': '../resources/05.jpg',
+        'description': 'Aenean in velit et tellus ullamcorper lacinia. Proin eu orci rutrum, mollis felis sed, vehicula magna. Aliquam erat volutpat. Sed dolor sem, rhoncus ut faucibus convallis, blandit sit amet diam.',
+        'price': 34.99,
+        'countInStock': 1000,
+        'rating': 4.5,
+        'numReviews': 10
+    },
+    {
+        'id': '6',
+        'name': 'Kamień ciemny',
+        'image': '../resources/06.jpg',
+        'description': 'Aenean in velit et tellus ullamcorper lacinia. Proin eu orci rutrum, mollis felis sed, vehicula magna. Aliquam erat volutpat. Sed dolor sem, rhoncus ut faucibus convallis, blandit sit amet diam.',
+        'price': 11.00,
+        'countInStock': 1000,
+        'rating': 4.5,
+        'numReviews': 10
+    },
+    {
+        'id': '7',
+        'name': 'Kamień żółty',
+        'image': '../resources/07.jpg',
+        'description': 'Aenean in velit et tellus ullamcorper lacinia. Proin eu orci rutrum, mollis felis sed, vehicula magna. Aliquam erat volutpat. Sed dolor sem, rhoncus ut faucibus convallis, blandit sit amet diam.',
+        'price': 34.99,
+        'countInStock': 1000,
+        'rating': 4.5,
+        'numReviews': 10
+    }
+    
+]
+
